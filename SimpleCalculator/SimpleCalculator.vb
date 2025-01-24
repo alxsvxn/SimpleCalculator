eaddfa53ab372dcd665a5d394c7fbfad20720384
Alexis Villagran <alexisvillagran18@hotmail.com>
2025-01-23 20:42:44 -0700

    Added Header, Renamed Module, & Added some code to Sub Main

---
+++ SimpleCalculator/SimpleCalculator.vb
@@ -0,0 +1,20 @@
+﻿'Alexis Villagran 
+'Spring 2025
+'RCET 2265
+'SimpleCalculator
+'https://github.com/alxsvxn/SimpleCalculator.git
+
+Module SimpleCalculator
+
+    Sub Main()
+
+        Console.WriteLine("Hello! Welcome to the BRAND NEW super-quantum-computing-super-computer Version 2.0")
+        Console.WriteLine()
+        Console.WriteLine("To start, we are going to need some numbers:")
+
+        Dim input1 As String
+        input1 = Console.ReadLine()
+
+    End Sub
+
+End Module
