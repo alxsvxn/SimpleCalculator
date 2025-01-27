bd9589bd46b7067e99c0b77ba54d7d03827a8f74
Alexis Villagran <alexisvillagran18@hotmail.com>
2025-01-26 20:23:43 -0700

    Added If statement code, rewrote choice code, put assigned variables in header

--- SimpleCalculator/SimpleCalculator.vb
+++ SimpleCalculator/SimpleCalculator.vb
@@ -8,20 +8,43 @@ Module SimpleCalculator
 
     Sub Main()
 
+        Dim input1 As String
+        Dim userInput As String
+        Dim input2 As String
+
         Console.WriteLine("Hello! Welcome to the BRAND NEW super-quantum-computing-super-computer Version 2.0")
         Console.WriteLine()
         Console.WriteLine("To start, we are going to need some numbers:")
 
-        Dim input1 As String 'This line assigns 'input1' as a string
         input1 = Console.ReadLine() 'This declares 'input1' as something that the user inputs
         Dim num1 As Integer = CInt(input1) 'This assigns num1 as an integer and takes the users input and converts it from a string to an integer
 
         Console.WriteLine("Great, now for our second number!")
 
-        Dim input2 As String = Console.ReadLine() 'This is a slightly simpler for of the code in line 15, but instead asks for an input and stores it in the same line
-        Dim num2 As Integer = CInt(input2) 'This is doing the same thing as line 17 as we still need to conver the string number to an integer
+        input2 = Console.ReadLine()
+        Dim num2 As Integer = CInt(input2) 'This is doing the same thing as line 17 as we still need to convert the string number to an integer
 
-        Console.WriteLine("Awesome, now would you like to Add or Mulitply them?")
+        Console.WriteLine("Would you like to: " & vbNewLine _ 'Making code more neat as to not exceed about 80Ch per line
+                          & "1. Add" & vbNewLine _
+                          & "2. Subtract" & vbNewLine _
+                          & "3. Multiply" & vbNewLine
+                          )
+        userInput = Console.ReadLine
+
+        Console.WriteLine($"You chose {userInput}.")
+
+        If userInput = "1" Then 'This is an if statement, allows for choice of multiple options
+            Console.WriteLine("You chose to Add.")
+            Console.WriteLine($"{num1} + {num2} = {num1 + num2}")
+        ElseIf userInput = "2" Then
+            Console.WriteLine("You chose to Subtract.")
+            Console.WriteLine($"{num1} - {num2} = {num1 - num2}")
+        ElseIf userInput = "3" Then
+            Console.WriteLine("You chose to Multiply.")
+            Console.WriteLine($"{num1} * {num2} = {num1 * num2}")
+        Else
+            Console.WriteLine("no.")
+        End If
 
     End Sub
 
